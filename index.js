b172046f2ff171a5bb8bb135cba92d1c9bb503a1
nekonok <nekonok.io@gmail.com>
2017-06-06 11:48:41 +0900

    remove linebreak-style rule

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 module.exports = {
   rules: {
     'strict': [2, 'function'],          // http://eslint.org/docs/rules/strict
-    'linebreak-style': [2, 'windows'],  // http://eslint.org/docs/rules/linebreak-style
     'operator-linebreak': [2, 'after'], // http://eslint.org/docs/rules/operator-linebreak
     'padded-blocks': 0,                 // http://eslint.org/docs/rules/padded-blocks
     'spaced-comment': 0,                // http://eslint.org/docs/rules/spaced-comment
