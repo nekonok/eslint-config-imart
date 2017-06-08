332e7056a016f2a8896a7b4cb3b75884e0bf9878
nekonok <nekonok.io@gmail.com>
2017-06-08 13:33:05 +0900

    apply eslint --fix

--- test/api-doc-mapping.js
+++ test/api-doc-mapping.js
@@ -1,6 +1,6 @@
 // test suite
-var chai = require('chai');
-var expect;
+const chai = require('chai');
+let expect;
 chai.use(require('chai-as-promised')); // for promise
 chai.use(require('chai-things')); // for array asserting
 chai.config.truncateThreshold = 0;  // disable truncating
@@ -11,16 +11,13 @@ const _ = require('lodash');
 const mapping = require('../lib/im-api-doc-mapping');
 
 describe('target API doc definitions', () => {
-  'use strict';
-  var targets = _(mapping.parsers).map('targets').flatten().value();
+  const targets = _(mapping.parsers).map('targets').flatten().value();
 
   it('should contain certain properties', () => {
     expect(targets).to.all.have.keys(['display', 'file', 'url']);
   });
 
   it('should have normalized "file" property', () => {
-    expect(targets).to.all.satisfy((t) => {
-      return t.file.match(/^(iap|iwp)-(server|client)-([a-z0-9_]+)$/);
-    });
+    expect(targets).to.all.satisfy(t => t.file.match(/^(iap|iwp)-(server|client)-([a-z0-9_]+)$/));
   });
 });
