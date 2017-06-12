683bcd00e6c2316f2de69f99627bacc2a265b8f9
nekonok <nekonok.io@gmail.com>
2017-06-12 11:36:52 +0900

    use eslint-plugin-prettier to fix

--- index.js
+++ index.js
@@ -2,24 +2,27 @@ module.exports = {
   rules: {
     // Airbnbではbabel等のtranspilerによるstrictモード埋め込みを期待しているため有効化
     // http://eslint.org/docs/rules/strict
-    strict: [2, 'function'],
+    strict: [2, "function"],
 
     // 演算子を行頭と行末のどちらに置くかを統一するため有効化
     // http://eslint.org/docs/rules/operator-linebreak
-    'operator-linebreak': [2, 'after'],
+    "operator-linebreak": [2, "after"],
 
     // Airbnb通常とlegacyでルールが異なるためlegacyに合わせる
-    'comma-dangle': [2, 'never'],
+    "comma-dangle": [2, "never"],
 
     // JSDocの記述を強制
     // http://eslint.org/docs/rules/require-jsdoc
-    'require-jsdoc': 2,
+    "require-jsdoc": 2,
 
     // JSDoc記述内容のチェック
     // http://eslint.org/docs/rules/valid-jsdoc
-    'valid-jsdoc': [2, {
-      requireParamDescription: false,
-      requireReturnDescription: false
-    }]
+    "valid-jsdoc": [
+      2,
+      {
+        requireParamDescription: false,
+        requireReturnDescription: false
+      }
+    ]
   }
 };
